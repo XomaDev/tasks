cd53e0bf52ed9806ed04886fe6a79ffcbc5a7792
XomaDev <bgpsdr@gmail.com>
2021-07-23 20:05:47 +0530

    Added events, foreground mode with improvements

--- src/xyz/kumaraswamy/tasks/ActivityService.java
+++ src/xyz/kumaraswamy/tasks/ActivityService.java
@@ -1,15 +1,21 @@
 package xyz.kumaraswamy.tasks;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
-import android.os.Handler;
-import android.os.Looper;
+import android.content.Context;
+import android.os.Build;
 import android.os.PersistableBundle;
 import android.util.Log;
+
 import com.google.appinventor.components.runtime.Component;
-import com.google.appinventor.components.runtime.TinyDB;
+import com.google.appinventor.components.runtime.errors.YailRuntimeError;
+import com.google.appinventor.components.runtime.util.JsonUtil;
 import com.google.appinventor.components.runtime.util.YailDictionary;
 import com.google.appinventor.components.runtime.util.YailList;
+import org.json.JSONException;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -17,21 +23,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import static xyz.kumaraswamy.tasks.Tasks.TAG;
-import static xyz.kumaraswamy.tasks.Tasks.TASK_CALL_FUNCTION;
-import static xyz.kumaraswamy.tasks.Tasks.TASK_CREATE_FUNCTION;
+import static xyz.kumaraswamy.tasks.Tasks.*;
 
 public class ActivityService extends JobService {
 
     private static final String TAG = "ActivityService";
     public static final String JOB = "job";
 
+    private boolean stopped = false;
+
     private int JOB_ID;
     private ComponentManager manager;
-    private TinyDB tinyDB;
 
     private final HashMap<String, Object[]> functions = new HashMap<>();
-
+    private final HashMap<String, ArrayList<Object>> events = new HashMap<>();
+    private final HashMap<String, Object[]> extraFunctions = new HashMap<>();
     @Override
     public boolean onStartJob(JobParameters parms) {
         Log.d(TAG, "onStartJob: Job started");
@@ -46,11 +52,10 @@ public class ActivityService extends JobService {
     private void processFunctions(final PersistableBundle extras) {
         JOB_ID = extras.getInt(JOB);
 
-        ComponentManager.ComponentsBuiltListener builtListener = () -> {
+        ComponentManager.ComponentsBuiltListener componentsCreated = () -> {
             Log.d(TAG, "componentsBuilt: Components build!");
-            YailDictionary pendingTasks = (YailDictionary) tinyDB.GetValue(Tasks.PENDING_TASKS, "");
-            String[] taskProcessList =
-                    ((ArrayList<?>) tinyDB.GetValue(Tasks.TASK_PROCESS_LIST, "")).toArray(new String[0]);
+            YailDictionary pendingTasks = (YailDictionary) getValue(Tasks.PENDING_TASKS, "");
+            String[] taskProcessList = ((ArrayList<?>) getValue(Tasks.TASK_PROCESS_LIST, "")).toArray(new String[0]);
 
             Log.d(TAG, "processFunctions: " + pendingTasks);
             Log.d(TAG, "processFunctions: " + Arrays.toString(taskProcessList));
@@ -61,18 +66,60 @@ public class ActivityService extends JobService {
             }
         };
 
-        new Handler(Looper.getMainLooper()).post(() -> {
-            manager = new ComponentManager(ActivityService.this, builtListener);
+        ComponentManager.EventRaisedListener eventRaisedListener = (component, eventName, parameters) -> {
+            if (!stopped) {
+                Log.d(TAG, "eventRaisedListener: Event raised of name " + eventName);
+            }
+        };
 
-            tinyDB = new TinyDB(manager.form());
-            tinyDB.Namespace(Tasks.TAG + JOB_ID);
+        Object components =  getValue(JOB, new HashMap<String, String>());
+        manager = new ComponentManager(this, (HashMap<String, String>)
+                components, componentsCreated, eventRaisedListener);
 
-            HashMap<String, String> components = components();
+        boolean foreground = extras.getBoolean(FOREGROUND_MODE);
+        Log.d(TAG, "processFunctions: Foreground " + foreground);
 
-            final Runnable runnable = () -> manager.createComponents(components);
-            final Thread thread = new Thread(runnable);
-            thread.start();
-        });
+        if (foreground) {
+            initialiseForeground(extras.getStringArray(FOREGROUND_CONFIG));
+        }
+    }
+
+    private void initialiseForeground(String[] values) {
+        Log.i(TAG, "initialiseForeground: " + values[2]);
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            String CHANNEL_ID = "BackgroundService";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Task",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).
+                    createNotificationChannel(channel);
+
+            final String icon = values[3];
+
+            final int iconInt = (icon.isEmpty() || icon.equalsIgnoreCase("DEFAULT"))
+                    ? android.R.drawable.ic_menu_info_details
+                    : Integer.parseInt(icon.replaceAll(" ", ""));
+
+            final Notification notification = new Notification.Builder(this, "BackgroundService")
+                    .setSubText(values[0])
+                    .setContentTitle(values[1])
+                    .setContentText(values[2])
+                    .setSmallIcon(iconInt)
+                    .build();
+
+            startForeground(1, notification);
+        }
+    }
+
+    private Object getValue(String tag, Object valueIfTagNotThere) {
+        try {
+            String value = getSharedPreferences(Tasks.TAG + JOB_ID, 0).getString(tag, "");
+            return value.length() == 0 ? valueIfTagNotThere : JsonUtil.getObjectFromJson(value, true);
+        } catch (JSONException exception) {
+            throw new YailRuntimeError("Value failed to convert from JSON.", "JSON Creation Error.");
+        }
     }
 
     private void processTasks(YailDictionary pd, String[] tasks) throws Exception {
@@ -84,7 +131,8 @@ public class ActivityService extends JobService {
             final int taskId = Integer.parseInt(taskData[0]);
             final int taskType = Integer.parseInt(taskData[1]);
 
-            Log.d(TAG, "Task Id: " + taskId + ", taskType: " + taskType);
+            final String message = "Task Id: " + taskId + ", taskType: " + taskType;
+            Log.d(TAG, message);
 
             Object[] taskValues = ((YailList) pd.get(taskId + "")).toArray();
             Log.d(TAG, "Task values: " + Arrays.toString(taskValues));
@@ -93,8 +141,27 @@ public class ActivityService extends JobService {
                 function(taskValues);
             } else if (taskType == TASK_CALL_FUNCTION) {
                 handleFunction(taskValues);
+            } else if (taskType == TASK_REGISTER_EVENT) {
+                putEventName(taskValues);
+            } else if (taskType == TASK_EXTRA_FUNCTION) {
+                extraFunction(taskValues);
+            }
+            else {
+                throw new Exception("Invalid task " + message);
             }
         }
+    }
+
+    private void extraFunction(Object[] taskValues) throws Exception {
+        String id = taskValues[0].toString();
+        Object[] values = (Object[]) taskValues[1];
+
+        if (!extraFunctions.containsKey(id)) {
+            Log.d(TAG, "extraFunction: Created function name " + id);
+            extraFunctions.put(id, values);
+            return;
+        }
+        throwFunctionExists(id);
     }
 
     private void function(Object[] taskValues) throws Exception {
@@ -106,7 +173,10 @@ public class ActivityService extends JobService {
             Log.d(TAG, "function: Created function name " + functionName);
             return;
         }
+        throwFunctionExists(functionName);
+    }
 
+    private void throwFunctionExists(String functionName) throws Exception {
         throw new Exception("The functions already contain the key \"" + functionName + "\".");
     }
 
@@ -114,14 +184,15 @@ public class ActivityService extends JobService {
         final Object[] taskValues = functions.get(values[0].toString());
 
         if (taskValues == null || taskValues.length == 0) {
-            Log.d(TAG, "Invalid invoke values provided");
+            Log.e(TAG, "Invalid invoke values provided");
             return;
         }
 
         final String key = taskValues[0].toString();
         final String methodName = taskValues[1].toString();
         final Object[] parms = ((YailList) taskValues[2]).toArray();
-        Log.d(TAG, "handleFunction: parms for the function" + Arrays.toString(parms));
+
+        Log.d(TAG, "handleFunction: parms for the function " + Arrays.toString(parms));
 
         final Component component = manager.component(key);
 
@@ -129,6 +200,7 @@ public class ActivityService extends JobService {
             Log.d(TAG, "handleFunction: The component \"" + key + "\" does not exists!");
             return;
         }
+
         try {
             Object result = MethodHandler.invokeComponent(component, methodName, parms);
             Log.d(TAG, "handleFunction: Invoke result: " + result);
@@ -137,16 +209,27 @@ public class ActivityService extends JobService {
         }
     }
 
-    private HashMap<String, String> components() {
-        return (HashMap<String, String>) tinyDB.GetValue(JOB, new HashMap<String, String>());
+    private void putEventName(Object[] taskValues) {
+        final String componentId = taskValues[0].toString();
+        final Object[] eventNameFunctionId = new Object[]{taskValues[1], taskValues[2]};
+
+        ArrayList<Object> eventsMap = events.getOrDefault(componentId, new ArrayList<>());
+        eventsMap.add(eventNameFunctionId);
+
+        events.put(componentId, eventsMap);
+        Log.d(TAG, "putEventName: Registered event name and function Id" + Arrays.toString(eventNameFunctionId));
     }
 
     @Override
     public boolean onStopJob(JobParameters parms) {
         Log.d(TAG, "onStopJob: Job cancelled");
+        stopped = true;
         return false;
     }
 }
+
+// SOME PART OF CODE FROM DYNAMIC_COMPONENTS
+// EXTENSION - AI2
 
 class MethodHandler {
     public static Object invokeComponent(final Component component, final String methodName, Object[] params) throws InvocationTargetException, IllegalAccessException {
@@ -181,7 +264,9 @@ class MethodHandler {
     private static Method findMethod(Method[] methods, String name, int parameterCount) {
         name = name.replaceAll("[^a-zA-Z0-9]", "");
         for (Method method : methods) {
-            if (method.getName().equals(name) && method.getParameterTypes().length == parameterCount) return method;
+            if (method.getName().equals(name)
+                    && method.getParameterTypes().length == parameterCount)
+                return method;
         }
         return null;
     }
--- src/xyz/kumaraswamy/tasks/Tasks.java
+++ src/xyz/kumaraswamy/tasks/Tasks.java
@@ -23,8 +23,9 @@ import com.google.appinventor.components.runtime.util.YailList;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static android.content.Context.ALARM_SERVICE;
+import static android.content.Context.JOB_SCHEDULER_SERVICE;
 import static xyz.kumaraswamy.tasks.ActivityService.JOB;
-import static xyz.kumaraswamy.tasks.ComponentManager.componentSource;
 
 public class Tasks extends AndroidNonvisibleComponent {
 
@@ -45,11 +46,20 @@ public class Tasks extends AndroidNonvisibleComponent {
 
     static final int TASK_CREATE_FUNCTION = 0;
     static final int TASK_CALL_FUNCTION = 1;
+    static final int TASK_REGISTER_EVENT = 2;
+    static final int TASK_EXTRA_FUNCTION = 3;
 
     public static final String PENDING_TASKS = "pending_tasks";
     public static final String TASK_PROCESS_LIST = "process_list";
+    public static final String FOREGROUND_MODE = "foreground_mode";
+    public static final String FOREGROUND_CONFIG = "foreground_config";
+
+    private static final String EXTRA_NETWORK = "extra_network";
 
     private boolean exact = false;
+    private String[] foreground = new String[] {
+            "Tasks", "Foreground service", "Task is running!", ""
+    };
 
     public Tasks(ComponentContainer container) {
         super(container.$form());
@@ -57,16 +67,19 @@ public class Tasks extends AndroidNonvisibleComponent {
         tinyDB = new TinyDB(container);
         activity = container.$context();
 
-        jobScheduler = (JobScheduler) activity.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        alarmManager = (AlarmManager) activity.getSystemService(Context.ALARM_SERVICE);
+        jobScheduler = (JobScheduler) activity.getSystemService(JOB_SCHEDULER_SERVICE);
+        alarmManager = (AlarmManager) activity.getSystemService(ALARM_SERVICE);
     }
 
     public static class AlarmReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.d(TAG, "onReceive: Received intent!");
-            JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-            startWork(intent.getIntExtra(JOB, 0), 0, context, jobScheduler);
+            JobScheduler jobScheduler = (JobScheduler) context.getSystemService(JOB_SCHEDULER_SERVICE);
+            startWork(intent.getIntExtra(JOB, 0), 0, context,
+                    jobScheduler, intent.getIntExtra(EXTRA_NETWORK, JobInfo.NETWORK_TYPE_NONE),
+                    intent.getBooleanExtra(FOREGROUND_MODE, false),
+                    intent.getStringArrayExtra(FOREGROUND_CONFIG));
         }
     }
 
@@ -81,35 +94,50 @@ public class Tasks extends AndroidNonvisibleComponent {
     }
 
     @SimpleFunction(description = "Starts the service.")
-    public boolean Start(final int id, long latency) {
+    public boolean Start(final int id, long latency, String requiredNetwork, boolean foreground) {
         tinyDB.Namespace(TAG + id);
 
         tinyDB.StoreValue(JOB, components);
         tinyDB.StoreValue(PENDING_TASKS, pendingTasks);
         tinyDB.StoreValue(TASK_PROCESS_LIST, YailList.makeList(tasksProcessList));
 
+        requiredNetwork = requiredNetwork.toUpperCase();
+        int network = JobInfo.NETWORK_TYPE_NONE;
+
+        if (requiredNetwork.equals("ANY")) {
+            network = JobInfo.NETWORK_TYPE_ANY;
+        } else if (requiredNetwork.equals("CELLULAR")) {
+            network = JobInfo.NETWORK_TYPE_CELLULAR;
+        } else if (requiredNetwork.equals("UNMETERED")) {
+            network = JobInfo.NETWORK_TYPE_UNMETERED;
+        } else if (requiredNetwork.equals("NOT_ROAMING")) {
+            network = JobInfo.NETWORK_TYPE_NOT_ROAMING;
+        }
+
         if (exact) {
             Log.d(TAG, "Start: Request for exact start!");
-            alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(
-                    System.currentTimeMillis() + latency, getReceiverIntent(id)
-            ), getReceiverIntent(id));
+            alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(System.currentTimeMillis() + latency,
+                    getReceiverIntent(id, network, foreground)), getReceiverIntent(id, network, foreground));
             return true;
         }
-        return startWork(id, latency, activity, jobScheduler);
+        return startWork(id, latency, activity, jobScheduler, network, foreground, this.foreground);
     }
 
-    private static boolean startWork(int id, long latency, Context context, JobScheduler jobScheduler) {
+    private static boolean startWork(int id, long latency, Context context, JobScheduler jobScheduler, int network,
+                                     boolean foreground, String[] foregroundConfig) {
         final PersistableBundle bundle = new PersistableBundle();
         bundle.putInt(JOB, id);
+        bundle.putBoolean(FOREGROUND_MODE, foreground);
+        bundle.putStringArray(FOREGROUND_CONFIG, foregroundConfig);
 
         ComponentName componentName = new ComponentName(context, ActivityService.class);
-        JobInfo.Builder builder =
-                new JobInfo.Builder(id, componentName);
+        JobInfo.Builder builder = new JobInfo.Builder(id, componentName);
 
         builder.setPersisted(true);
         builder.setMinimumLatency(latency);
         builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NONE);
         builder.setExtras(bundle);
+        builder.setRequiredNetworkType(network);
 
         int resultCode = jobScheduler.schedule(builder.build());
         boolean success = (resultCode == JobScheduler.RESULT_SUCCESS);
@@ -118,9 +146,12 @@ public class Tasks extends AndroidNonvisibleComponent {
         return success;
     }
 
-    public PendingIntent getReceiverIntent(int id) {
+    public PendingIntent getReceiverIntent(int id, int network, boolean foreground) {
         final Intent intent = new Intent(activity, AlarmReceiver.class);
         intent.putExtra(JOB, id);
+        intent.putExtra(EXTRA_NETWORK, network);
+        intent.putExtra(FOREGROUND_MODE, foreground);
+        intent.putExtra(FOREGROUND_CONFIG, this.foreground);
         return PendingIntent.getBroadcast(activity, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
@@ -131,24 +162,39 @@ public class Tasks extends AndroidNonvisibleComponent {
 
     @SimpleFunction(description = "Create components for the background use.")
     public void CreateComponent(final Object component, final String name) {
-        components.put(name, componentSource(component));
+        components.put(name, ComponentManager.getSourceString(component));
     }
 
     @SimpleFunction(description = "Creates the function that will be called in the background.")
-    public void CreateFunction(final String id, final String component, final String blockName, final YailList values) {
-        tasksProcessList.add(processTaskId + "/" + TASK_CREATE_FUNCTION);
-        pendingTasks.put(processTaskId, toObjectArray(component, blockName, values, id));
-        processTaskId++;
+    public void CreateFunction(final String id, final String component, final String block, final YailList values) {
+        put(TASK_CREATE_FUNCTION, component, block, values, id);
     }
 
     @SimpleFunction(description = "Calls the function in the background.")
     public void CallFunction(final String id) {
-        tasksProcessList.add(processTaskId + "/" + TASK_CALL_FUNCTION);
-        pendingTasks.put(processTaskId, toObjectArray(id));
-        processTaskId++;
+        put(TASK_CALL_FUNCTION, id);
     }
 
-    private Object[] toObjectArray(final Object... objects) {
-        return new ArrayList<>(Arrays.asList(objects)).toArray();
+    @SimpleFunction(description = "Registers the event for the component.")
+    public void RegisterEvent(String component, String eventName, String functionId) {
+        put(TASK_REGISTER_EVENT, component, eventName, functionId);
+    }
+
+    @SimpleFunction(description = "Creates an extra function.")
+    public void ExtraFunction(final String id, YailList codes) {
+        put(TASK_EXTRA_FUNCTION, id, codes.toArray());
+    }
+
+    @SimpleFunction(description = "Configures the foreground.")
+    public void ConfigureForeground(String title, String content, String subtitle, String icon) {
+        foreground = new String[] {
+                title, content, subtitle, icon
+        };
+    }
+
+    private void put(int task, Object... objects) {
+        tasksProcessList.add(processTaskId + "/" + task);
+        pendingTasks.put(processTaskId, new ArrayList<>(Arrays.asList(objects)).toArray());
+        processTaskId++;
     }
 }
